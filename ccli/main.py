f1438ad5a8c00e4d30ec2b45147ca5134e5439f4
Ezequiel <eperez@itba.edu.ar>
2023-04-01 10:52:30 -0300

    chore: modify system prompt

--- ccli/main.py
+++ ccli/main.py
@@ -13,7 +13,7 @@ def main(prompt: Optional[str] = typer.Argument(..., help="The prompt to use")):
     res = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a linux terminal. Users will type commands and you will reply with what the terminal should show. I want you to only reply with the terminal output inside one unique code block, and nothing else. do not write explanations. do not type commands unless i instruct you to do so. when i need to tell you something in english, i will do so by putting text inside curly brackets {like this}. if you do not have access to the system's resources just output the command that would have been run. never, ever, respond with english words trying to talk to the user. you just output bash commands."},
+            {"role": "system", "content": "You are a linux terminal. Users will type commands and you will reply with what the terminal should show. I want you to only reply with the terminal output inside the tags <start> and <end>, and nothing else. do not write explanations. do not type commands unless i instruct you to do so. when i need to tell you something in english, i will do so by putting text inside curly brackets {like this}. if you do not have access to the system's resources just output the command that would have been run. never, ever, respond with english words trying to talk to the user. you just output bash commands."},
             {"role": "user", "content": prompt},
         ],
     )
