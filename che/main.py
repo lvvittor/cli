34f460a91d9964c171077a32ab8d1d5034aa5e2e
Ezequiel <eperez@itba.edu.ar>
2023-04-01 13:50:31 -0300

    feat: add options to take when a response is obtained

--- che/main.py
+++ che/main.py
@@ -1,44 +1,94 @@
 from typing import Optional
 import openai
 import os
+import platform
 import typer
+import pyperclip
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 app = typer.Typer()
 
 
-def parse_output(ouput: str):
-    typer.echo(ouput)
+def is_unix_system():
+    return platform.system().lower().startswith("lin") or platform.system().lower().startswith("dar")
+
+
+# Show terminal options
+if is_unix_system():
+    from simple_term_menu import TerminalMenu
+elif platform.system().lower().startswith("win"):
+    import inquirer
 
 
 @app.command()
 def main(exe: bool = typer.Option(False), prompt: Optional[str] = typer.Argument("", help="The prompt to use")):  
     if exe:
         # API CALL, EXECUTABLES OR BASH SCRIPTS
-        typer.launch("./ccli/videoPlugin.sh")
+        typer.launch("./ccli/videoPlugin.sh") # Can use the return value of the script to do something
         return
+    
+    is_user_satisfied = None
 
-    res = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are an AI Terminal Copilot. Your job is to help users find the right terminal command in a bash shell on Linux. Just output the command the user is looking for, and nothing else. Do not make any comments, nor try to talk to the user in any way. Just output the bash command, iwthout any styling or comments. Don't add line breaks or quotes backticks or anything"},
-            {"role": "user", "content": prompt},
-        ],
-    )
+    while is_user_satisfied == None or not is_user_satisfied:
+        if (is_user_satisfied == False):
+            corrections = input("Indicaciones adicionales: ")
+            prompt += f". {corrections}"
+        res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are an AI Terminal Copilot. Your job is to help users find the right terminal command in a bash shell on Linux. Just output the command the user is looking for, and nothing else. Do not make any comments, nor try to talk to the user in any way. Just output the bash command, iwthout any styling or comments. Don't add line breaks or quotes backticks or anything. If more than one command is requested, concatenate them with && so that they run one after the other. If the user asks for a command that is not possible, just output 'El comportamiento pedido no es posible de realizar actualmente.' instead of any commands."},
+                {"role": "user", "content": prompt},
+            ],
+        )
 
-    answer = res["choices"][0]["message"]["content"]
+        answer = res["choices"][0]["message"]["content"]
+        explanation = None
 
-    commands = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are going to receive bash commands, along with other text. I want you to extract those bash commands, and output each one of them, separated by a \\n line break. Add a comment to explain in detail what the commands and their flags do."},
-            {"role": "user", "content": answer},
-        ],
-    )
+        is_user_satisfied = True
 
-    command = commands["choices"][0]["message"]["content"]
+        typer.echo(answer)
+        typer.echo("\n")
 
-    typer.echo(answer)
+        # What to do with the answer
 
-    typer.echo(command)
+        options = ["Ejecutar", "Copiar", "Explicar", "Corregir", "Salir"]
+
+        menu_entry_index = None
+
+        while menu_entry_index == None or options[menu_entry_index] == "Explicar":
+            if is_unix_system():
+                terminal_menu = TerminalMenu(options)
+                menu_entry_index = terminal_menu.show()
+            elif platform.system().lower().startswith("win"):
+                questions = [
+                    inquirer.List(
+                        "menu_entry_index",
+                        message="Acción a tomar",
+                        choices=options,
+                    ),
+                ]
+                answers = inquirer.prompt(questions)
+                menu_entry_index = options.index(answers["menu_entry_index"])
+
+            if menu_entry_index == 0:
+                os.system(answer)
+                typer.echo("\n")
+            elif menu_entry_index == 1:
+                print("> copied")
+                pyperclip.copy(answer)
+            elif menu_entry_index == 2:
+                if explanation == None:
+                    res = openai.ChatCompletion.create(
+                        model="gpt-3.5-turbo",
+                        messages=[
+                            {"role": "system", "content": "You are going to receive bash commands, along with other text. I want you to extract those bash commands, and output each one of them, separated by a \\n line break. Add a comment to explain in detail what the commands and their flags do."},
+                            {"role": "user", "content": answer},
+                        ],
+                    )
+                    explanation = res["choices"][0]["message"]["content"]
+
+                typer.echo(explanation)
+                typer.echo("\n")
+            elif menu_entry_index == 3:
+                is_user_satisfied = False
