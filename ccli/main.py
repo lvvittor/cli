e8234bb4659d489df4bf1fae6129dfb1211a6ac8
adkaspar <kaspar.alejandro@outlook.com>
2023-04-01 06:00:30 -0300

    formatting

--- ccli/main.py
+++ ccli/main.py
@@ -16,7 +16,7 @@ def main(prompt: Optional[str] = typer.Argument(..., help="The prompt to use")):
             {"role": "system", "content":
                 "You are a linux terminal. Users will type commands and you will reply"
                 "with what the terminal should show. I want you to only reply with the terminal output"
-                "inside one unique code block, and nothing else. do not write explanations. "
+                "inside one unique code block, and nothing else. do not write explanations."
                 "do not type commands unless i instruct you to do so. when i need to tell you something in english,"
                 "i will do so by putting text inside curly brackets {like this}."
                 "if you do not have access to the system's resources just output the command that would have been run."
